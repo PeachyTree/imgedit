a34cc1a5e0e18d7f547445e029c203004998259d
Azura Apple <dragonminner867@outlook.com>
2020-06-11 09:00:46 +0200

    Improved help command

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,19 @@
+// Copyright (©) 2020 Azura Apple. All rights reserved. MIT License.
+
+const { stripIntedient } = require('common-tags');
+
 exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
-  const guildConf = client.settings.get(message.guild.id) || client.defaults;
-  message.reply(`Imgedit Commands:\n9gag, bandicam, blur, blurple, brazzers, circle, deepfry, deviantart, disabled, explode, flip, flop, funky, haah, help, hooh, hypercam, ifunny, implode, invert, jpeg, magik, mc, memecenter, prefix, qrcreate, scott, sharpen, shutterstock, sonic, swirl, tile, trump, waaw, wall, wdt, wikihow, woow, wth`);
+  message.channel.send(stripIntedient`
+    __**Bot Commands**__
+    The default prefix is \`i.\`
+
+    __Image Manipulation:__
+    9gag, bandicam, blur, blurple, brazzers, circle, deepfry, deviantart, disabled, explode, flip, flop, funky, haah, hooh, hypercam, ifunny, implode, invert, jpeg, magik, mc, memecenter, qrcreate, scott, sharpen, shutterstock, sonic, swirl, tile, trump, waaw, wall, wdt, wikihow, woow, wth
+    
+    __Other:__
+    help, prefix
+    
+  `);
 };
 
 exports.aliases = [];
